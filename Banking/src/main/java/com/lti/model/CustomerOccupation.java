7f16e093dc731bf0c4f052bdf26120d385e69e38
Anonyo-Java-Training <anonyosanyal2@gmail.com>
2020-10-26 23:46:34 +0530

    customeroccupation error resolved

--- Banking/src/main/java/com/lti/model/CustomerOccupation.java
+++ Banking/src/main/java/com/lti/model/CustomerOccupation.java
@@ -1,13 +1,18 @@
 package com.lti.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name="tbl_customer_occupation")
 public class CustomerOccupation{
 
 	@Id
