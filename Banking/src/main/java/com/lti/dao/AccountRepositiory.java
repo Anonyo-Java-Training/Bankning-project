850162397cf5d59a805813c1a63a4a9ea4b6328d
code-daily-shubham <khatrishubham2197@gmail.com>
2020-10-27 15:14:30 +0530

    Account Implementation added

--- Banking/src/main/java/com/lti/dao/AccountRepositiory.java
+++ Banking/src/main/java/com/lti/dao/AccountRepositiory.java
@@ -4,5 +4,7 @@ import com.lti.model.Account;
 
 public interface AccountRepositiory {
 	 void createAccount(Account account);
+	 Account viewAccountDetails(long accountNumber);
+	 long showBalance(long accountNumber);
 
 }
